07f9b7f355e414ed5a7c5c2bc413ac7a31926578
Saad Shafique <hafizsaad1619@gmail.com>
2024-07-08 20:33:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "saad Shafique";
+console.log("Lowercase:", personName.toLowerCase());
+console.log("Upeercase:", personName.toUpperCase());
+console.log("Titlecase:", personName.replace(/\b\w/g, c => c.toUpperCase()));
